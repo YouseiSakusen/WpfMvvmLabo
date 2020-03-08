ea92d11e2bb2f9ee60c4eefec7ba411d221b859e
Youseisakusen <reilo_okita@elf-mission.net>
2020-03-08 23:51:34 +0900

    Dapperサンプル用のプロジェクトを追加。

---
+++ ImaZipperProto/DapperSample/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using MahApps.Metro.Controls;
+
+namespace DapperSample
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : MetroWindow
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+	}
+}
---
+++ ImaZipperProto/HalationGhostWpfViewModels/Behaviors/DataContextDisposeAction.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Windows;
+using Microsoft.Xaml.Behaviors;
+
+namespace HalationGhost.WinApps.Behaviors
+{
+	/// <summary>
+	/// DataContextをDisposeするTriggerActionを表します。
+	/// </summary>
+	public class DataContextDisposeAction : TriggerAction<FrameworkElement>
+	{
+		/// <summary>
+		/// Actionを実行します。
+		/// </summary>
+		/// <param name="parameter">パラメータを表すobject。</param>
+		protected override void Invoke(object parameter)
+		{
+			var disposable = this.AssociatedObject?.DataContext as IDisposable;
+			disposable?.Dispose();
+		}
+	}
+}
